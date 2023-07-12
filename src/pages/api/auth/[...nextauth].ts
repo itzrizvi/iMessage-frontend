3ca0ab199944aa5d15d87c1e82058b82de558283
itzrizvi <shahriar.me.work@gmail.com>
2023-07-12 21:55:57 +0600

    Next auth adapter type error fixed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
 
 const prisma = new PrismaClient();
 
 export default NextAuth({
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GoogleProvider({
             clientId: process.env.G_CLIENT_ID as string,
