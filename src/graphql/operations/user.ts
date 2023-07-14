8d8407be1aa882d8a8b3d286cfaad162868d0c25
itzrizvi <shahriar.me.work@gmail.com>
2023-07-14 22:57:25 +0600

    Auth component modified and GraphQL Client configured

---
+++ src/graphql/operations/user.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { gql } from "@apollo/client";
+
+export default {
+    Queries: {},
+    Mutations: {
+        createUsername: gql`
+            mutation CreateUsername($username: String!) {
+                createUsername(username: $username) {
+                    success
+                    error
+                }
+            }
+        `
+    },
+    Subscriptions: {
+
+    }
+};
