696796ff3fd21ee7b5ed734fd386c300f744c754
itzrizvi <shahriar.me.work@gmail.com>
2023-08-17 02:56:14 +0600

    Added Input UI and submit function

--- src/components/Chat/Feed/Messages/MessageInput.tsx
+++ src/components/Chat/Feed/Messages/MessageInput.tsx
@@ -1,6 +1,7 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Input } from "@chakra-ui/react";
 import { Session } from "next-auth";
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 
 interface MessageInputProps {
   session: Session;
@@ -8,9 +9,36 @@ interface MessageInputProps {
 }
 
 const MessageInput = ({ session, conversationId }: MessageInputProps) => {
+  const [messageBody, setMessageBody] = useState("");
+
+  const onSendMessage = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+    } catch (error: any) {
+      console.log("On Send Message Error", error);
+      toast.error(error?.message);
+    }
+  };
+
   return (
     <Box px={4} py={6} width="100%">
-      <form onSubmit={() => {}}></form>
+      <form onSubmit={() => {}}>
+        <Input
+          value={messageBody}
+          onChange={(e) => setMessageBody(e.target.value)}
+          size="md"
+          placeholder="Type new message"
+          resize="none"
+          _focus={{
+            boxShadow: "none",
+            border: "1px solid",
+            borderColor: "whiteAlpha.300",
+          }}
+          _hover={{
+            borderColor: "whiteAlpha.700",
+          }}
+        />
+      </form>
     </Box>
   );
 };
