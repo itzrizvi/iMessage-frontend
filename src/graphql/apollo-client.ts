59ac8eb9b66e66b42b3f5c9673d473f2a111dae1
itzrizvi <shahriar.me.work@gmail.com>
2023-08-27 03:56:19 +0600

    WS URL changed

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -13,7 +13,7 @@ const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
-          url: `${process.env.NEXT_API_WS_URL}`,
+          url: "https://imessage-backend-versionone.onrender.com/graphql/subscriptions",
           connectionParams: async () => ({
             session: await getSession(),
           }),
