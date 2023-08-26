b6389339e5b3d605a993c748f32bcd994a05ad39
itzrizvi <shahriar.me.work@gmail.com>
2023-08-27 03:09:02 +0600

    API URL AND WS URL MOVED TO ENV

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -5,7 +5,7 @@ import { createClient } from "graphql-ws";
 import { getSession } from "next-auth/react";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: process.env.NEXT_API_URL,
   credentials: "include",
 });
 
@@ -13,7 +13,7 @@ const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
-          url: "ws://localhost:4000/graphql/subscriptions",
+          url: `${process.env.NEXT_API_WS_URL}`,
           connectionParams: async () => ({
             session: await getSession(),
           }),
