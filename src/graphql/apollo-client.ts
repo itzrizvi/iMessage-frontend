0fb177405e0f81111b756fd3315af9d774616460
itzrizvi <shahriar.me.work@gmail.com>
2023-08-27 23:24:09 +0600

    Cookie Configuration Added

--- src/graphql/apollo-client.ts
+++ src/graphql/apollo-client.ts
@@ -5,7 +5,7 @@ import { createClient } from "graphql-ws";
 import { getSession } from "next-auth/react";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://imessage-backend-versionone.onrender.com/graphql",
   credentials: "include",
 });
 
@@ -13,7 +13,7 @@ const wsLink =
   typeof window !== "undefined"
     ? new GraphQLWsLink(
         createClient({
-          url: "ws://localhost:4000/graphql/subscriptions",
+          url: "wss://imessage-backend-versionone.onrender.com/graphql/subscriptions",
           connectionParams: async () => ({
             session: await getSession(),
           }),
